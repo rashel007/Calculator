8be3292f197c99ecc55b6eec529cbc64445b2580
Estique <estique@estiques-imac.local>
2016-10-02 13:01:32 +0600

    Initial Commit

---
+++ MyCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCalculator
+//
+//  Created by Estique on 10/2/16.
+//  Copyright © 2016 Estique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
